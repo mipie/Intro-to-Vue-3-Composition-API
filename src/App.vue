b738886ba3634073d3654e600135237f0591cfaf
Kyamitsu <kyamitsuk@gmail.com>
2026-05-25 23:17:40 -0400

    new step in learning vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { ref } from 'vue'
+import socksGreenImage from './assets/images/socks_blue.jpeg'
 
 const product = ref('Socks')
+const image = ref(socksGreenImage)
+const url = ref("https://google.com")
 
 </script>
 
@@ -9,7 +12,9 @@ const product = ref('Socks')
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">
-        <!-- image goes here -->
+        <a v-bind:href="url">
+          <img v-bind:src="image">
+        </a>
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
