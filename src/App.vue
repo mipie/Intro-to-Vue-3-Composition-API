e25f1da1ee724929157ca302079ec4d2b9120ec8
Kyamitsu <kyamitsuk@gmail.com>
2026-05-25 23:32:02 -0400

    new step in learning vuejs

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import socksGreenImage from './assets/images/socks_green.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
+const instock = ref(true)
+const inventory = ref(100)
+const onSale = ref(true)
 
 </script>
   
@@ -16,6 +19,10 @@ const image = ref(socksGreenImage)
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <p v-if="inventory > 10">In stock</p>
+        <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+        <p v-else>Out of stock</p>
+        <p v-if="onSale">On sale</p>
       </div>
     </div>
   </div>
