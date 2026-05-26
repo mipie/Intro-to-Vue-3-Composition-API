cecf1b814a7840f47f12dbd8537a3ec250bd934a
Kyamitsu <kyamitsuk@gmail.com>
2026-05-25 23:48:37 -0400

    new step in learning vuejs

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@ const product = ref('Socks')
 const image = ref(socksGreenImage)
 const inStock = true
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
+const variants = ref([
+  { id: 2234, color: 'green' },
+  { id: 2235, color: 'blue' }
+])
+const sizes = ref([10, 20, 30, 40, 50])
 </script>
   
 <template>
@@ -19,6 +24,11 @@ const details = ref(['50% cotton', '30% wool', '20% polyester'])
         <h1>{{ product }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
+        <ul>
+          <li v-for="detail in details"> {{ detail }}</li>
+        </ul>
+        <div v-for="variant in variants" :key="variant.id">{{ variant.color }}</div>
+        <div v-for="size in sizes">{{ size }}</div>
       </div>
     </div>
   </div>
