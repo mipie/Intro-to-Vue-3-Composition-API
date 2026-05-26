e4cc2f674e0335acf25b35bd9c1eff4f97151cad
Kyamitsu <kyamitsuk@gmail.com>
2026-05-26 01:36:46 -0400

    new step in learning vuejs

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+const props = defineProps({
+    details: {
+        type: Array,
+        required: true
+    }
+})
+
+</script>
+
+<template>
+    <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+</template>
