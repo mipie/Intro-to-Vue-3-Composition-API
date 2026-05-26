818d879c878ce0a232a9e3e99bd6f6d3923aa3a8
Kyamitsu <kyamitsuk@gmail.com>
2026-05-26 00:07:28 -0400

    new step in learning vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import socksGreenImage from './assets/images/socks_green.jpeg'
+import socksBlueImage from './assets/images/socks_blue.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
@@ -9,11 +10,16 @@ const inStock = true
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
 const variants = ref([
-  { id: 2234, color: 'green' },
-  { id: 2235, color: 'blue' },
+  { id: 2234, color: 'green', image: socksGreenImage },
+  { id: 2235, color: 'blue', image: socksBlueImage },
 ])
 
 const cart = ref(0)
+const addToCart = () => cart.value += 1
+const removeToCart = () => cart.value -= 1
+const updateImage = (variantImage) => {
+  image.value = variantImage
+}
 </script>
   
 <template>
@@ -34,10 +40,12 @@ const cart = ref(0)
         <div
           v-for="variant in variants"
           :key="variant.id"
+          @mouseover="updateImage(variant.image)"
         >
           {{ variant.color }}
         </div>
-        <button class="button">Add to Cart</button>
+        <button class="button" @click="addToCart">Add to Cart</button>
+        <button class="button" @click="removeToCart">Decrements</button>
       </div>
     </div>
   </div>
